48d45720a9f1527880bfa5496e52cd10fd14ab43
Margarida Santini Szabo <margarida.szabo@alumni.usp.br>
2025-09-22 19:49:01 -0300

    Projeto de CRUD.

--- Program.cs
+++ Program.cs
@@ -1,6 +1,9 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using Microsoft.Extensions.FileProviders;// Qual diretório ele vai direcionar (pasta view)
+using MySql.Data.MySqlClient;
+using ProjetoExample.Model;//usa a var no aspnet recebendo o valor
 
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();// criação e configuração do app, modelo minimalista do aspnet core
 //app.MapGet("/", () => "Hello World!");
 //rota
 string connStr = "Server = localhost; Database = bd_loja; User = root"; // Password = admin;
@@ -9,6 +12,30 @@ string connStr = "Server = localhost; Database = bd_loja; User = root"; // Passw
 // Direcionar rota da view (páginas estáticas) e colocar ações do CRUD
 app.UseStaticFiles(new StaticFileOptions{
     // (new StaticFileOptions) intância
-
+    FileProvider = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), "Views")),//dois parâmetros e um método
+    RequestPath = ""
 });
+
+// direcionar pro index
+app.MapGet("/", () => Results.Redirect("/index.html"));
+
+app.MapGet("/produtos",()=>{ //referenciar para qual objeto a gente vai usar, rota get, lista que ele vai retornar
+    //conectar com o banco de dados através de uma string de conexão
+    var produtos = new List<Produto>();// lista é parecida com vetor
+    using var conn = new MySqlConnection(connStr);//control espaço
+    conn.Open();// abre conexão do BD
+    var cmd = new MySqlCommand("SELECT * FROM tb_produtos", conn);// comando sql
+    var reader = cmd.ExecuteReader();// executa o comando
+    while(reader.Read()){//enquanto tiver dados ele vai ler
+        produtos.Add(new Produto
+        {
+            Id = reader.GetInt32("id"),//pega o id do banco de dados
+            Nome = reader.GetString("nome"),//pega o nome do banco de dados
+            Preco = reader.GetDouble("preco"),//pega o preço do banco de dados
+            Categoria = reader.GetString("categoria")//pega a categoria do banco de dados
+
+        });//adiciona o produto na lista
+    }
+    return produtos;
+    });
 app.Run();
